c48016cad5713762626645004b27c7999128507d
LittleBlue418 <hollyathomas88@gmail.com>
2020-02-12 13:58:51 +0100

    Adding ItemList class for GET items

--- code/app.py
+++ code/app.py
@@ -20,9 +20,11 @@ class Item(Resource):
         return item, 201
 
 
-
+class ItemList(Resource):
+    def get(self):
+        return {'items': items}
 
 api.add_resource(Item, '/item/<string:name>')
-# http://127.0.0.1:5000/item/Hamster
+api.add_resource(ItemList, '/items')
 
 app.run(port=5000)

a85311039e58b94539542eaffa75dad519a68d87
LittleBlue418 <hollyathomas88@gmail.com>
2020-02-13 15:13:10 +0100

    tracking users

--- code/app.py
+++ code/app.py
@@ -2,9 +2,11 @@ from flask import Flask, request
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
+app.secret_key = 'key'
 api = Api(app)
 
 items = []
+
 
 class Item(Resource):
     def get(self, name):

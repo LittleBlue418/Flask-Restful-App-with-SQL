0aac159350349dafae3a01e086fd8d3a45f162a3
LittleBlue418 <hollyathomas88@gmail.com>
2020-02-19 13:28:26 +0100

    script to build our database tables

---
+++ code/create_tables.py
@@ -0,0 +1,13 @@
+import sqlite3
+
+connection = sqlite3.connect('data.db')
+cursor = connection.cursor()
+
+# Auto incrementing id, now we only have to specify the username and
+# the password, the INTEGER PRIMARY KEY does the heavy lifting
+create_table = "CREATE TABLE IF NOT EXIST users (id INTEGER PRIMARY KEY, username text, password text)"
+cursor.execute(create_table)
+
+connection.commit()
+
+connection.close()

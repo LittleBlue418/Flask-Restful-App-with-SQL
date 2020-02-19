bb9ba478d74ed354ab0107b3f4f1314ac04fa2dd
LittleBlue418 <hollyathomas88@gmail.com>
2020-02-19 14:24:43 +0100

    bug fix

--- code/create_tables.py
+++ code/create_tables.py
@@ -5,7 +5,7 @@ cursor = connection.cursor()
 
 # Auto incrementing id, now we only have to specify the username and
 # the password, the INTEGER PRIMARY KEY does the heavy lifting
-create_table = "CREATE TABLE IF NOT EXIST users (id INTEGER PRIMARY KEY, username text, password text)"
+create_table = "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username text, password text)"
 cursor.execute(create_table)
 
 connection.commit()

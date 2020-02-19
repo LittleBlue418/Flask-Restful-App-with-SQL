5e0969573d423654cd95e8d70e666b0ecafe447b
LittleBlue418 <hollyathomas88@gmail.com>
2020-02-19 17:18:51 +0100

    remove test item

--- code/create_tables.py
+++ code/create_tables.py
@@ -8,10 +8,9 @@ cursor = connection.cursor()
 create_table = "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username text, password text)"
 cursor.execute(create_table)
 
+# Creating out item table when we create our user table
 create_table = "CREATE TABLE IF NOT EXISTS items (name text, price real)"
 cursor.execute(create_table)
-
-cursor.execute("INSERT INTO items VALUES ('test', 19.99)")
 
 connection.commit()
 
